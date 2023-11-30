c529e3103961a9fb13b2120e40a76e17635edc3b
Eli <ebuddy456@gmail.com>
2023-11-30 17:29:57 -0600

    changes

--- components/script.js
+++ components/script.js
@@ -1,11 +1,7 @@
 function HandleColorChange(){
     var textElement = document.getElementById("Texttochange");
 
-    console.log(textElement)
-    for (const element of textElement){
-        element.style.color = "blue";
+    textElement.style.color = "blue"
 
-
-    }
 
 }
